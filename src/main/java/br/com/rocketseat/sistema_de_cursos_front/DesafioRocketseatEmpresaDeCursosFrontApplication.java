0f4dea849ebe5e79aaefcae950a3364f74a1fabf
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2025-01-06 16:23:50 -0300

    Criando projeto

---
+++ src/main/java/br/com/rocketseat/sistema_de_cursos_front/DesafioRocketseatEmpresaDeCursosFrontApplication.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.sistema_de_cursos_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioRocketseatEmpresaDeCursosFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioRocketseatEmpresaDeCursosFrontApplication.class, args);
+	}
+
+}
